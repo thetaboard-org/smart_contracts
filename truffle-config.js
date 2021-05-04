2946a40f93db5dd53b53d98142c0a8d599bbe2b9
maurinlenglart <maurin.lenglart@gmail.com>
2021-05-04 15:39:49 -0700

    contracts with BC and ABI

--- truffle-config.js
+++ truffle-config.js
@@ -43,10 +43,15 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
+     host: "https://theta-node-rpc-smart-contract-sandbox.thetatoken.org/rpc",     // Localhost (default: none)
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
+    test:{
+      host: "https://theta-node-rpc-smart-contract-sandbox.thetatoken.org/rpc",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "test",       // Any network (default: none)
+    }
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -82,7 +87,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.3",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.7.4",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
